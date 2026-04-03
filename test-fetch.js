1ac56fe80840d90bde6ad51574ed6a332225e0c4
HieuCuteDangYeu <nguyendinhhieu050505@gmail.com>
2026-04-03 16:51:23 +0700

    Remove test fetch file

--- test-fetch.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.EXPO_PUBLIC_SUPABASE_URL, process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
-async function run() {
-  const { data, error } = await supabase.from('expenses').select('*');
-  console.log({data, error});
-}
-run();
